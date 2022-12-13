82969e73dc45300e30413579bc5fbccfe3486129
femdawgs <99158516+femdawgs@users.noreply.github.com>
2022-12-13 14:27:20 -0500

    open payment

--- src/components/Femdawgs-Web.vue
+++ src/components/Femdawgs-Web.vue
@@ -512,7 +512,7 @@
           </div>
           
           <div class="mint-padding">
-            <img src="https://pro.nft-maker.io/images/buttons/paybutton_1_1.svg" @click="openPaymentWindow()">
+            <img src="https://pro.nft-maker.io/images/buttons/paybutton_2_3.svg" @click="openPaymentWindow()">
           </div>
 
         </div>
@@ -546,31 +546,31 @@
 
   export default{
 
-    // methods:{
-    //   openPaymentWindow() {
-    //   // Please replace this URL with the one from your own project. You have been warned!!!
-    //   const paymentUrl = `https://payment.nft-maker.io/?p=9ce0376a700c4eaab95db6a872a1a903&c=${document.querySelector('#nft-quantity').value}`;
-    //   // Specify the popup width and height
-    //   const popupWidth = 500;
-    //   const popupHeight = 700;
-    //   // Calculate the center of the screen
-    //   const left = window.top.outerWidth / 2 + window.top.screenX - (popupWidth / 2);
-    //   const top = window.top.outerHeight / 2 + window.top.screenY - (popupHeight / 2);
-    //   const popup = window.open(paymentUrl, "NFT-MAKER PRO Payment Gateway", `popup=1, location=1, width=${popupWidth}, height=${popupHeight}, left=${left}, top=${top}`);
-    //   // Show dim background
-    //   document.body.style = "background: rgba(0, 0, 0, 0.5)";
-    //   // Continuously check whether the popup has been closed
-    //   const backgroundCheck = setInterval(function() {
-    //     if (popup.closed) {
-    //       clearInterval(backgroundCheck);
-    //       console.log("Popup closed");
-    //       // Remove dim background
-    //       document.body.style = "";
-    //     }
-    //   }, 1000);
-    //   }
+    methods:{
+      openPaymentWindow() {
+      // Please replace this URL with the one from your own project. You have been warned!!!
+      const paymentUrl = "https://pay.nmkr.io/?p=9d264a5d12234f468a0722d5de924553&c=1";
+      // Specify the popup width and height
+      const popupWidth = 500;
+      const popupHeight = 700;
+      // Calculate the center of the screen
+      const left = window.top.outerWidth / 2 + window.top.screenX - (popupWidth / 2);
+      const top = window.top.outerHeight / 2 + window.top.screenY - (popupHeight / 2);
+      const popup = window.open(paymentUrl, "NFT-MAKER PRO Payment Gateway", `popup=1, location=1, width=${popupWidth}, height=${popupHeight}, left=${left}, top=${top}`);
+      // Show dim background
+      document.body.style = "background: rgba(0, 0, 0, 0.5)";
+      // Continuously check whether the popup has been closed
+      const backgroundCheck = setInterval(function() {
+        if (popup.closed) {
+          clearInterval(backgroundCheck);
+          console.log("Popup closed");
+          // Remove dim background
+          document.body.style = "";
+        }
+      }, 1000);
+      }
 
-    // }
+    }
   }
 
 
